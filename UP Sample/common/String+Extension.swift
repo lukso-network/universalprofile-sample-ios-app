8ec385072473fc65e0898704fda5e34eef68d622
Jenea Vranceanu <jeneavranceanu@gmail.com>
2021-09-23 00:59:49 +0300

    String now extended with Identifiable

---
+++ UP Sample/common/String+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  String+Extension.swift
+//  UP Sample
+//
+//  Created by JeneaVranceanu.
+//  LUKSO Blockchain GmbH © 2021
+//
+
+import Foundation
+
+extension String: Identifiable {
+    public var id: String {
+        return self
+    }
+}
